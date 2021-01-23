31ada3080b39e2e22e71f4734a79f78314bd3dc9
Henri Parviainen <henri.parviainen@icloud.com>
2021-01-23 08:08:17 +0200

    removed titlebar and renamed the app

--- public/electron.js
+++ public/electron.js
@@ -5,13 +5,16 @@ const path = require("path");
 const isDev = require("electron-is-dev");
 let mainWindow;
 function createWindow() {
-mainWindow = new BrowserWindow({ width: 1080, height: 710, icon: path.join(__dirname, "icon.png")});
+mainWindow = new BrowserWindow({ titleBarStyle: 'hidden', title: 'Hue Lite', width: 1080, height: 710, icon: path.join(__dirname, "icon.png")});
 mainWindow.loadURL(
 isDev
 ? "http://localhost:3000"
 : `file://${path.join(__dirname, "../build/index.html")}`
 );
 mainWindow.on("closed", () => (mainWindow = null));
+mainWindow.on('page-title-updated', function(e) {
+    e.preventDefault()
+  });
 }
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
